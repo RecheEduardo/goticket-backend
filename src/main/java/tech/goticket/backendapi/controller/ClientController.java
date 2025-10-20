e93e8f622306bd41f57c05e5c3ecf84fe5bdc7a7
recheeduardo <eduardorechemartins2006@gmail.com>
2025-10-20 17:58:52 -0300

    feat: adding JWT authentication return on successfull client POST 201 endpoint

--- src/main/java/tech/goticket/backendapi/controller/ClientController.java
+++ src/main/java/tech/goticket/backendapi/controller/ClientController.java
@@ -3,12 +3,16 @@ package tech.goticket.backendapi.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 import tech.goticket.backendapi.controller.dto.CreateClientDTO;
+import tech.goticket.backendapi.controller.dto.LoginResponse;
 import tech.goticket.backendapi.entities.Client;
 import tech.goticket.backendapi.entities.Role;
 import tech.goticket.backendapi.entities.User;
@@ -19,6 +23,7 @@ import tech.goticket.backendapi.repository.UserRepository;
 import tech.goticket.backendapi.repository.UserStatusRepository;
 import tech.goticket.backendapi.services.ClientService;
 
+import java.net.URI;
 import java.time.Instant;
 import java.time.LocalDate;
 
@@ -29,21 +34,23 @@ public class ClientController {
     private final ClientService clientService;
     private final UserStatusRepository userStatusRepository;
     private final BCryptPasswordEncoder passwordEncoder;
+    private final JwtEncoder jwtEncoder;
 
     public ClientController(UserRepository userRepository,
                             RoleRepository roleRepository,
                             ClientService clientService,
-                            UserStatusRepository userStatusRepository, BCryptPasswordEncoder passwordEncoder) {
+                            UserStatusRepository userStatusRepository, BCryptPasswordEncoder passwordEncoder, JwtEncoder jwtEncoder) {
         this.userRepository = userRepository;
         this.roleRepository = roleRepository;
         this.clientService = clientService;
         this.userStatusRepository = userStatusRepository;
         this.passwordEncoder = passwordEncoder;
+        this.jwtEncoder = jwtEncoder;
     }
 
     @PostMapping("/clients")
     @Transactional
-    public ResponseEntity<Void> createNewClient(@RequestBody CreateClientDTO dto) {
+    public ResponseEntity<LoginResponse> createNewClient(@RequestBody CreateClientDTO dto) {
 
         boolean isCpf = ClientService.isCPF(dto.identityDocument());
         if (!isCpf) { throw new ResponseStatusException(HttpStatus.BAD_REQUEST); }
@@ -73,6 +80,18 @@ public class ClientController {
 
         clientService.saveClient(client);
 
-        return ResponseEntity.ok().build();
+        var expiresIn = 300L;
+
+        var claims = JwtClaimsSet.builder()
+                .issuer("goticketbackend")
+                .subject(client.getUserID().toString())
+                .issuedAt(now)
+                .expiresAt(now.plusSeconds(expiresIn))
+                .build();
+
+        var jwtValue = jwtEncoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
+
+        return ResponseEntity.created(URI.create("/clients/" + client.getUserID()))
+                .body(new LoginResponse(jwtValue, expiresIn));
     }
 }
