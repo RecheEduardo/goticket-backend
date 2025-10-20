5c7480ebd696bc1a8ce1c10408eb21eab9d2a3ad
recheeduardo <eduardorechemartins2006@gmail.com>
2025-10-20 20:44:45 -0300

    feat: add security users list GET method using spring security funcs

--- src/main/java/tech/goticket/backendapi/controller/TokenController.java
+++ src/main/java/tech/goticket/backendapi/controller/TokenController.java
@@ -1,19 +1,23 @@
 package tech.goticket.backendapi.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.jwt.JwtClaimsSet;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import tech.goticket.backendapi.controller.dto.LoginRequest;
 import tech.goticket.backendapi.controller.dto.LoginResponse;
+import tech.goticket.backendapi.entities.User;
 import tech.goticket.backendapi.repository.UserRepository;
 
 import java.time.Instant;
+import java.util.List;
 
 @RestController
 public class TokenController {
@@ -42,15 +46,26 @@ public class TokenController {
         var now = Instant.now();
         var expiresIn = 300L;
 
+        var scope = user.get().getRole().getName();
+
         var claims = JwtClaimsSet.builder()
                 .issuer("goticketbackend")
                 .subject(user.get().getUserID().toString())
                 .issuedAt(now)
                 .expiresAt(now.plusSeconds(expiresIn))
+                .claim("scope", scope)
                 .build();
 
         var jwtvalue = jwtEncoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
 
         return ResponseEntity.ok(new LoginResponse(jwtvalue, expiresIn));
+    }
+
+    @GetMapping("/users")
+    @PreAuthorize("hasAuthority('SCOPE_ADMIN')")
+    public ResponseEntity<List<User>> listAllUsers(){
+        var users = userRepository.findAll();
+
+        return ResponseEntity.ok(users);
     }
 }
