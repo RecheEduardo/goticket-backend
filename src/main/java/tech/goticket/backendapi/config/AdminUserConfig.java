7007a9caa2e1fabf4163ec4b55a214daa3455270
recheeduardo <eduardorechemartins2006@gmail.com>
2025-10-20 17:12:23 -0300

    hotifx: setting instant admin profile creation with email instead of user

--- src/main/java/tech/goticket/backendapi/config/AdminUserConfig.java
+++ src/main/java/tech/goticket/backendapi/config/AdminUserConfig.java
@@ -45,7 +45,7 @@ public class AdminUserConfig implements CommandLineRunner {
                 },
                 () -> {
                     var user = new User();
-                    user.setEmail("admin");
+                    user.setEmail("admin@admin.com");
                     user.setPassword(bCryptPasswordEncoder.encode("123"));
                     user.setRole(roleAdmin);
                     user.setStatus(adminStatus);
