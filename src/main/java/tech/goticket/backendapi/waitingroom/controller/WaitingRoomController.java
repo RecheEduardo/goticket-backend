5b2b27726d3f7f1ec773e90f949d0bc8e1d99126
JoaoRamosVS <jvsrdez@hotmail.com>
2026-06-11 11:53:29 -0300

    fix: WaitingRoomController imports

--- src/main/java/tech/goticket/backendapi/waitingroom/controller/WaitingRoomController.java
+++ src/main/java/tech/goticket/backendapi/waitingroom/controller/WaitingRoomController.java
@@ -7,6 +7,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 import tech.goticket.backendapi.waitingroom.dto.QueueStatusResponse;
+import tech.goticket.backendapi.waitingroom.service.WaitingRoomService;
 
 import java.util.UUID;
 
